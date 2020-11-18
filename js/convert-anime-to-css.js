019b35853985859107afbcc0a2e9e6b2afbb75f9
Sam Muirhead <sam@cameralibre.cc>
2020-11-18 16:16:53 +1300

    Split functions into smaller parts

--- js/convert-anime-to-css.js
+++ js/convert-anime-to-css.js
@@ -11,84 +11,33 @@
 let cumulativeTimelineDuration = 0
 const targets = {}
 
-const logAnimations = (anim) => {
+function logAnimations (anim) {
   const sectionOffset = anim.timelineOffset
   for (const animation of anim.animations) {
     const targetId = animation.animatable.target.id
-    // Without an id, keyframes will be written for an empty target.
-    // This should probably throw an error, rather than log to the console:
-    if (!targetId) console.log('WARNING: missing id property for ' + animation.animatable.target.tagName + ' element in #' + animation.animatable.target.parentNode.id)
-
     const animatedProperty = animation.property
     const fromValues = []
     const toValues = []
 
+    // Without an id, keyframes will be written for an empty target.
+    if (!targetId) console.log('WARNING: missing id property for ' + animation.animatable.target.tagName + ' element in #' + animation.animatable.target.parentNode.id)
+
     for (const tween of animation.tweens) {
       const tweenStart = tween.start + tween.delay + sectionOffset
       const tweenEnd = tween.end + sectionOffset
-
-      // anime adds 'px' to strokeDashoffset values for some reason
-      if (animatedProperty === 'strokeDashoffset') {
-        fromValues.push(tween.from.numbers[0])
-        toValues.push(tween.to.numbers[0])
-      } else { // otherwise anime's extra formatting (eg. +px, +deg) is quite useful
-        fromValues.push(tween.from.original)
-        toValues.push(tween.to.original)
-      }
-
-      // make a new ruleset for the current target, if there isn't one already
-      if (!targets[targetId]) targets[targetId] = { }
-
-      // add an empty keyframe at the tweenStart time, if there isn't a keyframe there already
-      if (!targets[targetId][tweenStart]) targets[targetId][tweenStart] = { }
-
-      // add an empty keyframe at the tweenEnd time, if there isn't a keyframe there already
-      if (!targets[targetId][tweenEnd]) targets[targetId][tweenEnd] = { }
-
       const validTransforms = ['translateX', 'translateY', 'translateZ', 'rotate', 'rotateX', 'rotateY', 'rotateZ', 'scale', 'scaleX', 'scaleY', 'scaleZ', 'skew', 'skewX', 'skewY', 'perspective', 'matrix', 'matrix3d']
-      // check if animatedProperty is a transform
+
+      writeFromAndToValues(tween, animatedProperty, fromValues, toValues)
+      createKeyframeObjects(targets, targetId, tweenStart, tweenEnd)
+
       if (validTransforms.includes(animatedProperty)) {
-        // add a 'transform' object to the starting keyframe, if it doesn't exist yet
-        if (!targets[targetId][tweenStart].transform) targets[targetId][tweenStart].transform = { }
-
-        // add the animated property and its value to the transform object, if it doesn't exist yet
-        if (!targets[targetId][tweenStart].transform[animatedProperty]) {
-          Object.defineProperty(targets[targetId][tweenStart].transform, animatedProperty, {
-            value: fromValues[fromValues.length - 1],
-            enumerable: true
-          })
-        }
-        // if it does exist already, um, do nothing for now...
-        // TODO: find out what is happening if it does exist!
-
-        // add a 'transform' object to the ending keyframe, if it doesn't exist yet
-        if (!targets[targetId][tweenEnd].transform) targets[targetId][tweenEnd].transform = { }
-
-        // add the animated property and its value to the transform object, if it doesn't exist yet
-        if (!targets[targetId][tweenEnd].transform[animatedProperty]) {
-          Object.defineProperty(targets[targetId][tweenEnd].transform, animatedProperty, {
-            value: toValues[toValues.length - 1],
-            enumerable: true
-          })
-        }
-      } else { // if animatedProperty is not a transform
-        // if the current property name isn't found in the starting keyframe, add it, and its starting value
-        if (!targets[targetId][tweenStart][animatedProperty]) {
-          Object.defineProperty(targets[targetId][tweenStart], animatedProperty, {
-            value: fromValues[fromValues.length - 1],
-            enumerable: true
-          })
-        }
-        // if the current property name isn't found in the ending keyframe, add it, and its starting value
-        if (!targets[targetId][tweenEnd][animatedProperty]) {
-          Object.defineProperty(targets[targetId][tweenEnd], animatedProperty, {
-            value: toValues[toValues.length - 1],
-            enumerable: true
-          })
-        }
-        cumulativeTimelineDuration = Math.round((animation.duration + sectionOffset) * 100 + Number.EPSILON) / 100
-        console.log('timeline duration:', cumulativeTimelineDuration)
+        addValuesToTransformObject(targets, targetId, animatedProperty, tweenStart, tweenEnd, fromValues, toValues)
+      } else {
+        addValuesToPropertyObject(targets, targetId, animatedProperty, tweenStart, tweenEnd, fromValues, toValues)
       }
+
+      cumulativeTimelineDuration = Math.round((animation.duration + sectionOffset) * 100 + Number.EPSILON) / 100
+      console.log('timeline duration:', cumulativeTimelineDuration)
     }
   }
 }
@@ -99,59 +48,137 @@ function timelineComplete () {
   const targetIds = Object.keys(targets)
 
   targetIds.forEach(id => {
-    const absoluteKeyframes = Object.keys(targets[id])
-    let percentageKeyframe = ''
-    // convert absolute time keyframes to percentages
-    absoluteKeyframes.forEach(keyframe => {
-      percentageKeyframe = Math.round(keyframe / cumulativeTimelineDuration * 100 * 100 + Number.EPSILON) / 100 + '%'
-
-      targets[id][percentageKeyframe] = targets[id][keyframe]
-      delete targets[id][keyframe]
-    })
-    const percentageKeyframes = Object.keys(targets[id])
-    // add a 0% & 100% keyframe to each animation so they start in the
-    // correct initial position and hold at the end
-
-    // Keyframe -> Number
-    const firstKeyframeNumber = (kf) => {
-    // takes keyframe string (e.g. '0%, 19%')
-    // returns first number in this string as an integer (e.g. 0)
-      const first = kf
-        .split(',')[0]
-        .replace('%', '')
-      return Number(first)
-    }
-
-    const oldFirstKeyframe = percentageKeyframes[0]
-    const oldLastKeyframe = percentageKeyframes[percentageKeyframes.length - 1]
-    if (oldFirstKeyframe !== '0%') {
-      const newFirstKeyframe = '0%, ' + oldFirstKeyframe
-      targets[id][newFirstKeyframe] = targets[id][oldFirstKeyframe]
-      delete targets[id][oldFirstKeyframe]
-    }
-    if (oldLastKeyframe !== '100%') {
-      const newLastKeyframe = oldLastKeyframe + ', 100%'
-      targets[id][newLastKeyframe] = targets[id][oldLastKeyframe]
-      delete targets[id][oldLastKeyframe]
-    }
-    // build a new keyframes object where the keys are sorted by percentage
-    // 0% (or 0,n%) should be first, 100 (or n%, 100%) should be last.
-    const sortedKeyframes = Object.keys(targets[id])
-      .sort((a, b) => firstKeyframeNumber(a) - firstKeyframeNumber(b))
-      .reduce((keyframes, keyframe) => {
-        // starting with an empty object, run through each key, in our sorted lisst
-        // add it to the obj with the value being that same key in our targets[id]
-        // when we are done, we'll have a full sorted object
-        return {
-          ...keyframes,
-          [keyframe]: targets[id][keyframe]
-        }
-      }, {}) // here be the empty object that starts the reduce loop
-    targets[id] = sortedKeyframes
+    convertToPercentageKeyframes(targets, id)
+    addStartAndEndKeyframes(targets, id)
   })
 
+  convertObjectToCss(targets, targetIds)
+}
+
+function writeFromAndToValues (tween, animatedProperty, fromValues, toValues) {
+  // anime adds 'px' to strokeDashoffset values for some reason
+  if (animatedProperty === 'strokeDashoffset') {
+    fromValues.push(tween.from.numbers[0])
+    toValues.push(tween.to.numbers[0])
+  } else { // otherwise anime's extra formatting (eg. +px, +deg) is quite useful
+    fromValues.push(tween.from.original)
+    toValues.push(tween.to.original)
+  }
+}
+
+function createKeyframeObjects (targets, targetId, tweenStart, tweenEnd) {
+  // make a new ruleset for the current target, if there isn't one already
+  if (!targets[targetId]) targets[targetId] = { }
+  // add an empty keyframe at the tweenStart time, if there isn't a keyframe there already
+  if (!targets[targetId][tweenStart]) targets[targetId][tweenStart] = { }
+  // add an empty keyframe at the tweenEnd time, if there isn't a keyframe there already
+  if (!targets[targetId][tweenEnd]) targets[targetId][tweenEnd] = { }
+}
+
+function addValuesToTransformObject (targets, targetId, animatedProperty, tweenStart, tweenEnd, fromValues, toValues) {
+  // add a 'transform' object to the starting keyframe, if it doesn't exist yet
+  if (!targets[targetId][tweenStart].transform) targets[targetId][tweenStart].transform = { }
+
+  // add the animated property and its value to the transform object, if it doesn't exist yet
+  if (!targets[targetId][tweenStart].transform[animatedProperty]) {
+    Object.defineProperty(targets[targetId][tweenStart].transform, animatedProperty, {
+      value: fromValues[fromValues.length - 1],
+      enumerable: true
+    })
+  }
+  // if it does exist already, um, do nothing for now...
+  // TODO: find out what is happening if it does exist!
+
+  // add a 'transform' object to the ending keyframe, if it doesn't exist yet
+  if (!targets[targetId][tweenEnd].transform) targets[targetId][tweenEnd].transform = { }
+
+  // add the animated property and its value to the transform object, if it doesn't exist yet
+  if (!targets[targetId][tweenEnd].transform[animatedProperty]) {
+    Object.defineProperty(targets[targetId][tweenEnd].transform, animatedProperty, {
+      value: toValues[toValues.length - 1],
+      enumerable: true
+    })
+  }
+}
+
+function addValuesToPropertyObject (targets, targetId, animatedProperty, tweenStart, tweenEnd, fromValues, toValues) {
+  // if the current property name isn't found in the starting keyframe, add it, and its starting value
+  if (!targets[targetId][tweenStart][animatedProperty]) {
+    Object.defineProperty(targets[targetId][tweenStart], animatedProperty, {
+      value: fromValues[fromValues.length - 1],
+      enumerable: true
+    })
+  }
+  // if the current property name isn't found in the ending keyframe, add it, and its starting value
+  if (!targets[targetId][tweenEnd][animatedProperty]) {
+    Object.defineProperty(targets[targetId][tweenEnd], animatedProperty, {
+      value: toValues[toValues.length - 1],
+      enumerable: true
+    })
+  }
+}
+
+function convertToPercentageKeyframes (targets, id) {
+  const absoluteKeyframes = Object.keys(targets[id])
+  let percentageKeyframe = ''
+
+  // convert absolute time keyframes to percentages
+  absoluteKeyframes.forEach(keyframe => {
+    percentageKeyframe = Math.round(keyframe / cumulativeTimelineDuration * 100 * 100 + Number.EPSILON) / 100 + '%'
+
+    targets[id][percentageKeyframe] = targets[id][keyframe]
+    delete targets[id][keyframe]
+  })
+}
+
+function sortKeyframes (targets, id) {
+  // Keyframe -> Number
+  const firstKeyframeNumber = (kf) => {
+  // takes keyframe string (e.g. '0%, 19%')
+  // returns first number in this string as an integer (e.g. 0)
+    const first = kf
+      .split(',')[0]
+      .replace('%', '')
+    return Number(first)
+  }
+  // build a new keyframes object where the keys are sorted by percentage
+  // 0% (or 0,n%) should be first, 100 (or n%, 100%) should be last.
+  const sortedKeyframes = Object.keys(targets[id])
+    .sort((a, b) => firstKeyframeNumber(a) - firstKeyframeNumber(b))
+    .reduce((keyframes, keyframe) => {
+      // starting with an empty object, run through each key, in our sorted lisst
+      // add it to the obj with the value being that same key in our targets[id]
+      // when we are done, we'll have a full sorted object
+      return {
+        ...keyframes,
+        [keyframe]: targets[id][keyframe]
+      }
+    }, {}) // here be the empty object that starts the reduce loop
+  targets[id] = sortedKeyframes
+}
+
+function addStartAndEndKeyframes (targets, id) {
+  // add a 0% & 100% keyframe to each animation so they start in the
+  // correct initial position and hold at the end
+
+  const percentageKeyframes = Object.keys(targets[id])
+  const oldFirstKeyframe = percentageKeyframes[0]
+  const oldLastKeyframe = percentageKeyframes[percentageKeyframes.length - 1]
+  if (oldFirstKeyframe !== '0%') {
+    const newFirstKeyframe = '0%, ' + oldFirstKeyframe
+    targets[id][newFirstKeyframe] = targets[id][oldFirstKeyframe]
+    delete targets[id][oldFirstKeyframe]
+  }
+  if (oldLastKeyframe !== '100%') {
+    const newLastKeyframe = oldLastKeyframe + ', 100%'
+    targets[id][newLastKeyframe] = targets[id][oldLastKeyframe]
+    delete targets[id][oldLastKeyframe]
+  }
+  sortKeyframes(targets, id)
+}
+
+function convertObjectToCss (targets, targetIds) {
   let animDetails = JSON.stringify(targets)
-  // here comes some verbose and repetitive regex!
 
   // remove quotation marks
   animDetails = animDetails.replace(/['"]+/g, '')
